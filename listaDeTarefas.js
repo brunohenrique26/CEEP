285a16af80ae5d3eaac254a40d1b94725268d5e8
Bruno Henrique <b.henrique261991@gmail.com>
2021-12-16 17:43:37 -0200

    Adicionando tarefa

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -2,20 +2,22 @@ const criarTarefa = (evento) => {
 
     evento.preventDefault()
 
-    const input = document.querySelector("[data-form-input]")
-    const valor = input.value 
-    console.log(valor)
-    
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
+    const valor = input.value
 
-    const tarefa = document.querySelector("[data-task]")
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    
     const conteudo = `<p class="content">${valor}</p>`
 
     tarefa.innerHTML = conteudo
 
-    input.value = ""
+    lista.appendChild(tarefa)
+    input.value = " "
+
 }
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener("click", criarTarefa )
-
+novaTarefa.addEventListener('click', criarTarefa)
